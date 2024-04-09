ad4cacffbd93aa8fc139aa4748cffd3959e6edd2
Javier Izquierdo Portillo <j.izquierdoportillo@gmail.com>
2024-04-09 19:23:46 +0200

    Mensajes de suma y resta añadidos

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -5,6 +5,9 @@ public class PruebaGit {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hola, soy un tío guay haciendo un repositorio");
+		System.out.println("6+7="+suma(5,7));
+		System.out.println("6-7="+resta(5,7));
+		
 	}
 	
 	public static int suma (int a, int b) {
